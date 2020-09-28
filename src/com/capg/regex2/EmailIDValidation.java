c45335ac297d06525527161709620ea6807d7627
beni2412 <hardaman97@gmail.com>
2020-09-28 19:43:50 +0530

    [Hardaman] Added: Validate the mandatory part and start with abc

--- src/com/capg/regex2/EmailIDValidation.java
+++ src/com/capg/regex2/EmailIDValidation.java
@@ -1,9 +1,21 @@
 package com.capg.regex2;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EmailIDValidation {
 	public static void main(String[] args) {
-		System.out.println("Welcome to email validation");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter an email: ");
+		String email = sc.nextLine();
+		Pattern pattern =Pattern.compile("^abc");
+		Matcher matcher = pattern.matcher(email);
+		boolean matchfound = matcher.find();
+		if(matchfound) {
+			System.out.println("Match found");
+		}
+		else {
+			System.out.println("Match not found");
+		}
 	}
 }
-
-
